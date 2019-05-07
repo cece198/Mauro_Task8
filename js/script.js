d56283f08352ff1c60056fd1c2577971490d1164
Cecelia <cece198@gmail.com>
2019-05-07 19:14:46 -0400

    Added on click function for the button (and tested)

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+$('.btn').on('click', function(){
+    var apiBase = 'https://jsonplaceholder.typicode.com/photos';
+
+    alert("hi");
+
+    //axios.get(apiBase).then(function(response){
+
+    //});
+});
