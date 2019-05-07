e8f6915d8946caf2ae9bc2976dde548a9caaee38
Cecelia <cece198@gmail.com>
2019-05-07 19:27:51 -0400

    On click, Axios GET response

--- js/script.js
+++ js/script.js
@@ -2,9 +2,12 @@
 $('.btn').on('click', function(){
     var apiBase = 'https://jsonplaceholder.typicode.com/photos';
 
-    alert("hi");
+    //alert("hi");
 
-    //axios.get(apiBase).then(function(response){
-
-    //});
+    axios.get(apiBase).then(function(response){
+    //console.log(response);
+        var imageHTML = response.data.map(function(image){
+            console.log(image);
+        });
+    });
 });
