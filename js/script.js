99f3504b212b3b9eddc71121034e8a1c2b13261a
Cecelia <cece198@gmail.com>
2019-05-07 20:09:28 -0400

    Testing if text gets printed to webpage

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
 
 $('.btn').on('click', function(){
-    var apiBase = 'https://jsonplaceholder.typicode.com/photos';
+    var apiBase = 'https://jsonplaceholder.typicode.com/photos/';
 
-    //alert("hi");
 
     axios.get(apiBase).then(function(response){
     //console.log(response);
         var imageHTML = response.data.map(function(image){
             console.log(image);
+
+            //For each item in the loop, output a div containing an image <img> with the URL as its source 
+                //and the title as its alt tag, underneath the <img> tag, a <p> tag with the responses’ title.
+
+            var $title = $('<h2>').html('Title: ');
+
+            $('#tasks').html($title);
+
+            //title
+            //thumbnailUrl
+            //return $('<img>')
         });
+        //$('#tasks').html('loading.....');
     });
 });
