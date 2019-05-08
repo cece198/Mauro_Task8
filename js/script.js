a9d257f80f0efe74a7e112909e35f4b74c35d752
Cecelia <cece198@gmail.com>
2019-05-07 20:47:50 -0400

    Testing if the first part of the output prints in the console

--- js/script.js
+++ js/script.js
@@ -4,21 +4,18 @@ $('.btn').on('click', function(){
 
 
     axios.get(apiBase).then(function(response){
-    //console.log(response);
         var imageHTML = response.data.map(function(image){
-            console.log(image);
+            //For each item in the loop, output a div containing an image 
+            //<img> with the URL as its source and the title as its alt tag, 
+            //underneath the <img> tag, a <p> tag with the responses’ title.
+            
+            //console.log(image);
+            
+            var img = '<img src="' + image.url + '" alt= "' + image.title + '">';
+            console.log(img);
 
-            //For each item in the loop, output a div containing an image <img> with the URL as its source 
-                //and the title as its alt tag, underneath the <img> tag, a <p> tag with the responses’ title.
-
-            var $title = $('<h2>').html('Title: ');
-
-            $('#tasks').html($title);
-
-            //title
-            //thumbnailUrl
-            //return $('<img>')
+            //return $('#tasks').addClass('image').data('title', image.title).html(image.albumId);
         });
-        //$('#tasks').html('loading.....');
+        $('#tasks').html('loading.....');
     });
 });
